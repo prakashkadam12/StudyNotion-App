43deb942b9a6887b416140807fd9eb0bb120f336
parthmern <parthmern@gmail.com>
2024-01-01 13:20:44 -0500

    update section controller coded

--- server/controllers/Section.js
+++ server/controllers/Section.js
@@ -1,11 +1,12 @@
 const Section = require("../models/Section");
 const Course = require("../models/Course");
 
-
+// ===============================================
+// createSection
 exports.createSection = async (req, res) =>{
     try{
         // data fetch
-        const {name, courseId} = req.body ;
+        const {sectionName, courseId} = req.body ;
 
         // validation
         if(!sectionName || !courseId){
@@ -73,3 +74,62 @@ exports.createSection = async (req, res) =>{
 
     }
 }
+
+
+// ============================================
+// updateSection
+
+exports.updateSection = async (req, res) =>{
+    try{
+
+        // data fetch
+        const {sectionName, sectionId} = req.body ;
+
+        // validation
+        if(!sectionName || !sectionId){
+            return(
+                res.status(400).json(
+                    {
+                        success : false ,
+                        message : "All field are required" ,
+                    }
+                )
+            )
+        }
+
+        // update data
+        const section = await Section.findByIdAndUpdate(sectionId, {sectionName}, {new : true} );
+
+
+        // return
+        return(
+            res.status(200).json(
+                {
+                    success : true ,
+                    message : "update section successfully",
+                    section,
+                }
+            )
+        )
+
+        
+    }
+    catch(error){
+        console.log("error in update section ==>", error);
+
+        return(
+            res.status(500).json(
+                {
+                    success : false,
+                    message : "error in update section",
+                    error ,
+                }
+            )
+        )
+        
+    }
+}
+
+// ===============================================
+// delete section
+
