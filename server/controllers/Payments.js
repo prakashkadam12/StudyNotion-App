b64ffef258ae783c4122182054a53f0dc1d28614
parthmern <parthmern@gmail.com>
2024-01-02 19:49:28 -0500

    signature verification code

--- server/controllers/Payments.js
+++ server/controllers/Payments.js
@@ -6,7 +6,7 @@ const Course = require("../models/Course");
 const User = require("../models/User");
 const { default: mongoose } = require("mongoose");
 
-
+// ==============================================
 // CAPTUREPAYMENT
 exports.capturePayment = async (req, res) =>{
 
@@ -120,7 +120,112 @@ exports.capturePayment = async (req, res) =>{
 
 }
 
+// ====================================================
+// ==============================================
+// VERIFY SIGNATURE  -- ye request razorpay se aayegi (razorpay webhooks)
+
+exports.verifySignature = async (req, res) =>{
+         
+        const webhookSecret = "123456789"; // Your webhook secret obtained from Razorpay
+
+        const signature = req.headers("x-razorpay-signature"); // Extracting signature from request headers
+        
+        const shasum = crypto.createHmac("sha256", webhookSecret); // Creating an HMAC object using the webhook secret
+        
+        shasum.update(JSON.stringify()); // Updating the HMAC with request data
+        
+        const digest = shasum.digest("hex"); // Generating the digest (hash) in hexadecimal format
+        
+        // trying to authorized SIGNATURE
+        if(signature === digest){
+            console.log("payment is authorized");
+
+            // finding the "NOTES" object that we sended in razorpay capture payment
+            const {courseId, userId} = req.body.payload.payment.entity.notes ;
+
+            try{
+                // fullfill the action
+
+                // find the course and enrolled the student in it
+                const enrolledCourse = await Course.findOneAndUpdate(
+                    {_id : courseId},
+                    {$push : {studentsEnrolled : userId}},
+                    {new : true},
+                )
+
+                if(!enrolledCourse){
+                    return(
+                        res.status(500).json(
+                            {
+                                success : false ,
+                                message : "course not found" ,
+                            }
+                        )
+                    )
+                }
+
+                console.log("enrolledCourse ==>", enrolledCourse);
+
+                // find the student and add the course to their list of enrolled courses
+                const enrolledStudent = await User.findOneAndUpdate(
+                    {_id : userId},
+                    {$push : {courses : courseId}},
+                    {new : true},
+                )
+
+                console.log("enrolledStudent ==>", enrolledStudent);
+
+                // send mail of confirmation
+                const emailResponse = await mailSender(
+                                        enrolledStudent.email ,
+                                        "congratulations for buying course" ,
+                                        "<<<<mail template here>>>>"
+                );
+
+                console.log("email response=>", emailResponse);
+
+                return(
+                    res.status(200).json(
+                        {
+                            success : true ,
+                            message : "Signature verified and course added",
+
+                        }
+                    )
+                )
 
 
+
+
+            }
+            catch(error){
+                console.log("error in signature verified ==> ", error);
+                return(
+                    res.status(500).json(
+                        {
+                            success : false,
+                            message : "error in signature verified",
+                            error ,
+                        }
+                    )
+                )
+
+            }
+
+
+        }
+
+        else{
+            return(
+                res.status(500).json(
+                    {
+                        success : false,
+                        message : "signature not verified",
+                    }
+                )
+            )
+        }
+
+}
 
 
