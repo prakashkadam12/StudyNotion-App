a882e0548d4c5d8cbeca1a4367cc8f0befab1341
parthmern <parthmern@gmail.com>
2024-01-03 17:00:10 -0500

    getAllRating controller coded

--- server/controllers/RatingAndReview.js
+++ server/controllers/RatingAndReview.js
@@ -163,3 +163,53 @@ exports.getAverageRating = async (req, res) =>{
         )
     }
 }
+
+
+// =========================================================
+// getAllRating
+
+exports.getAllRating = async (req, res) =>{
+    try{
+
+        const allReviews = await RatingAndReview.find({
+            $sort : ({
+                rating : "desc"
+            })
+        })
+        .populate({
+            path : "user",
+            select : "firstName lastName email image",
+        }) 
+        .populate({
+            path : "course" ,
+            select : "courseName" ,
+        })
+        .exec() ;
+
+
+        return(
+            res.status(200).json(
+                {
+                    success : true ,
+                    message : "all reviews fetched successfully" ,
+                    data : allReviews ,
+                }
+            )
+        )
+
+
+    }
+    catch(error){
+        console.log("error in getAllRating ==>", error);
+        return(
+            res.status(500).json(
+                {
+                    success : true ,
+                    message : "error in getAllRating" ,
+                    error ,
+                }
+            )
+        )
+
+    }
+}
