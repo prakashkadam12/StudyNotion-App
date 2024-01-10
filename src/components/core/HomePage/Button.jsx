8bd49a774c4547d349084800ea8822ef06df2d21
parthmern <parthmern@gmail.com>
2024-01-09 23:36:38 -0500

    homepage

---
+++ src/components/core/HomePage/Button.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Button = ({children, active, linkto}) =>{
+
+    return(
+        <Link to={linkto}>
+            
+            <div className={`text-center text-[13px] px-6 py-3 `}>
+                {children}
+            </div>
+            
+        </Link>
+    )
+}
+
+export default Button ;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { FaArrowRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import HighlightText from "../components/core/HomePage/HighlightText";
+
+const Home = () =>{
+
+    return(
+        <div>
+            homepage
+            {/* section 1 */}
+            <div className="max-w-maxContent relative mx-auto flex flex-col w-11/12 item-center text-white justify-between ">
+                
+                <Link to={"/signup"}>
+
+                    <div className="group mt-16 p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200 transition-all duration-200 hover:scale-95 w-fit ">
+                        <div className="flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900 ">
+                            <p>Become and Instructor</p>
+                            <FaArrowRight />
+                        </div>
+                    </div>
+                    
+                </Link>
+            
+                <div className="text-center text-4xl font-semibold mt-7">
+                    Empower Your Future with 
+                    <HighlightText text={" Coding Skills"} />
+                </div>
+
+                <div className="w-[90%] text-center text-lg font-bold text-richblack-300 mt-4 ">
+                    With our online coding courses, you can learn at your own pace, from anywhere in the world, and get access to a wealth of resources, including hands-on projects, quizzes, and personalized feedback from instructors.
+                </div>
+
+                <div className="flex flex-row gap-7 mt-8">
+                    <CTAButton>
+                        Learn More
+                    </CTAButton>
+                </div>
+
+            </div>
+
+            {/* section 2 */}
+            {/* section 3 */}
+            {/* footer */}
+        </div>
+
+    )
+}
+
+export default Home ;
