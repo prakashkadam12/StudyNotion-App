3bd87016f8a441c7dcea8a52978047aac0609c86
parthmern <parthmern@gmail.com>
2024-01-19 12:16:22 -0500

    section 3 done about us

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,7 @@ import BannerImage1 from "../assets/Images/aboutus1.webp" ;
 import BannerImage2 from "../assets/Images/aboutus2.webp" ;
 import BannerImage3 from "../assets/Images/aboutus3.webp" ;
 import Quote from "../components/core/AboutPage/Quote";
+import FoundingStory from "../assets/Images/FoundingStory.png";
 
 const About = () => {
 
@@ -34,6 +35,49 @@ const About = () => {
             <section>
                 <div>
                     <Quote />
+                </div>
+            </section>
+
+            {/* section 3 */}
+            <section>
+                <div className="flex-col">
+
+                    {/* founding story upper wala box */}
+                    <div className="flex">
+                    
+                        {/* left box founding story */}
+                        <div>
+                            <h1>Our Founding Story</h1>
+                            <p>Our e-learning platform was born out of a shared vision and passion for transforming education. It all began with a group of educators, technologists, and lifelong learners who recognized the need for accessible, flexible, and high-quality learning opportunities in a rapidly evolving digital world.</p>
+                            <p>As experienced educators ourselves, we witnessed firsthand the limitations and challenges of traditional education systems. We believed that education should not be confined to the walls of a classroom or restricted by geographical boundaries. We envisioned a platform that could bridge these gaps and empower individuals from all walks of life to unlock their full potential.</p>
+                        </div>
+
+                        {/* founding story right box */}
+                        <div>
+                            <img src={FoundingStory} alt="" />
+                        </div>
+
+                    </div>
+
+                    {/* vision and mission */}
+                    <div className="flex">
+
+                        {/* left box - our vision */}
+                        <div>
+                            <h1>Our Vision</h1>
+                            <p>With this vision in mind, we set out on a journey to create an e-learning platform that would revolutionize the way people learn. Our team of dedicated experts worked tirelessly to develop a robust and intuitive platform that combines cutting-edge technology with engaging content, fostering a dynamic and interactive learning experience.</p>
+                        </div>
+
+                        {/* right box- our mission */}
+                        <div>
+                            <h1>Our Mission</h1>
+                            <p>Our mission goes beyond just delivering courses online. We wanted to create a vibrant community of learners, where individuals can connect, collaborate, and learn from one another. We believe that knowledge thrives in an environment of sharing and dialogue, and we foster this spirit of collaboration through forums, live sessions, and networking opportunities.</p>
+                        </div>
+
+
+
+                    </div>
+
                 </div>
             </section>
 
