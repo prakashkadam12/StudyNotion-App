394b2824df31a345c3813077b8cbc92d83e10370
parthmern <parthmern@gmail.com>
2024-01-20 23:49:41 -0500

    about page added

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -4,7 +4,28 @@ const Error = () =>{
 
     return(
         <div className="flex item-center justify-center text-5xl text-yellow-100">
-            Page not found
+            <figure>
+	
+	<figcaption>
+		<span className="sr-text">Error 404: Not Found</span>
+		<span className="e"></span>
+		<span className="r"></span>
+		<span className="r"></span>
+		<span className="o"></span>
+		<span className="r"></span>
+		<span className="_4"></span>
+		<span className="_0"></span>
+		<span className="_4"></span>
+		<span className="n"></span>
+		<span className="o"></span>
+		<span className="t"></span>
+		<span className="f"></span>
+		<span className="o"></span>
+		<span className="u"></span>
+		<span className="n"></span>
+		<span className="d"></span>
+	</figcaption>
+</figure>
         </div>
     )
 }
