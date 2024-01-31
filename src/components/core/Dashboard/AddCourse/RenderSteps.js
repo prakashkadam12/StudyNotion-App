747c764b379edbfd9800d73feffaec28e9ec603b
parthmern <parthmern@gmail.com>
2024-01-31 11:42:19 -0500

    courseSlice added

---
+++ src/components/core/Dashboard/AddCourse/RenderSteps.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const RenderSteps = () =>{
+
+    const { step } = useSelector((state) => state.course)
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default RenderSteps ;
