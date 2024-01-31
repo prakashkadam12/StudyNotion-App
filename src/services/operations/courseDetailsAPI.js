8bbdfa2063244a2e2363d1ca924e3cc1438484a0
parthmern <parthmern@gmail.com>
2024-01-31 16:32:18 -0500

    tags added in form section

--- src/services/operations/courseDetailsAPI.js
+++ src/services/operations/courseDetailsAPI.js
@@ -1,6 +1,7 @@
 import { toast } from "react-hot-toast"
 
-import { updateCompletedLectures } from "../../slices/viewCourseSlice"
+// TODO: remove comments
+// import { updateCompletedLectures } from "../../slices/viewCourseSlice"
 // import { setLoading } from "../../slices/profileSlice";
 import { apiConnector } from "../apiconnector"
 import { courseEndpoints } from "../apis"
