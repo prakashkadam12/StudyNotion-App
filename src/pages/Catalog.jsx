7e088394768ce08552e93a6e3320bc8ed161913a
parthmern <parthmern@gmail.com>
2024-02-04 00:29:58 -0500

    updates on catalog

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -4,9 +4,48 @@ import React from "react";
 const Catalog = () =>{
 
     return(
-        <div>
-            Catalog
-        </div>
+        <>
+            {/* staring of catalog page */}
+            <div>
+
+                <p>
+
+                </p>
+
+                <p>
+
+                </p>
+
+                <p>
+
+                </p>
+
+            </div>
+
+            {/* section 1 - Courses to get you started */}
+            <div className=" mx-auto box-content w-full max-w-maxContentTab px-4 py-12 lg:max-w-maxContent">
+                
+                <div className="section_heading">Courses to get you started</div>
+
+                <div className="my-4 flex border-b border-b-richblack-600 text-sm">
+                    
+                    {/* selection slider */}
+                    <p>
+                        Most Popular
+                    </p>
+
+                    <p>
+                        New
+                    </p>
+
+                </div>
+                
+                {/* course Slider */}
+                
+
+            </div>
+        
+        </>
     )
 }
 
