d0c4a3cb6be2257368fb78e84864f062fe6860f1
parthmern <parthmern@gmail.com>
2024-02-10 11:43:50 -0500

    courseDetails page state managements done

--- src/pages/CourseDetails.jsx
+++ src/pages/CourseDetails.jsx
@@ -1,10 +1,13 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { buyCourse } from "../services/operations/studentFeaturesAPI";
 import { useNavigate, useParams } from "react-router-dom";
+import GetAvgRating from "../utils/avgRating";
 
+import ConfirmationModal from "../components/common/ConfirmationModal";
 
+import Error from "../pages/Error";
 
 const CourseDetails = () =>{
 
@@ -14,9 +17,60 @@ const CourseDetails = () =>{
 
     const {token} = useSelector((state)=> state.auth);
     const {user} = useSelector((state)=> state.profile);
+    const {loading} = useSelector((state)=> state.profile);
+    const {paymentLoading} = useSelector((state)=> state.course);
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const {courseId} = useParams(); 
+
+    const [confirmationModal, setConfirmationModal] = useState(null);
+
+    const [courseData, setCourseData] = useState(null);
+
+    useEffect(()=>{
+
+        // getting course details
+        const getCourseFullDetails = async () =>{
+
+            try{
+
+                const result = await fetchCourseDetails(courseId); 
+                console.log("result of getFUllcoursedetails=>", result);
+                setCourseData(result);
+
+            
+            }
+            catch(error){
+                console.log("error in fetching details of course=>", error);
+    
+            }
+        }
+
+        getCourseFullDetails();
+
+    }, [courseId])
+
+    const [avgReviewCount, setAverageReviewCount] = useState(0);
+
+    useEffect(()=>{
+
+        const count = GetAvgRating(courseData?.data?.CourseDetails?.ratingAndReviews);
+        console.log("count avg rating=>", count);
+        setAverageReviewCount(count);
+
+    }, [courseData]);
+
+    const [totalNoOfLectures, setTotalNoOfLectures] = useState(0);
+    useEffect(()=>{
+
+        // finding total lectures
+        let lectures = 0 ;
+        response?.data?.CourseDetails?.courseContent?.forEach((sec) => {
+            lectures += sec.subSection.length || 0 ;
+        });
+
+        setTotalNoOfLectures(lectures);
+    }, [courseData]);
 
     const handleBuyCourse =() =>{
         console.log(courseId);
@@ -27,17 +81,41 @@ const CourseDetails = () =>{
         else{
             console.log("token is not there", token);
         }
+
+        // showing modal that saying u are not loggedin
+        setConfirmationModal({
+            text1 : "you are not loggedin",
+            text2 : "pleasr login to purchase the course", 
+            btn1Text : "Login" ,
+            btn2Text : "Calcel" ,
+            btn1Handler : () => navigate("/login"),  
+            btn2Handler : () => setConfirmationModal(null) ,
+        })
+
     }
     //console.log("token=>",token);
 
+    if(loading || !courseData){
+        return(
+            <div>
+                Loading . . 
+            </div>
+        )
+    }
+
+    if(!courseData.success){
+        return(
+            <div>
+                <Error />
+            </div>
+        )
+    }
+
     return(
-        <div className="text-white">
-            course details page
+        <div className="flex items-center">
 
-            <button className="bg-yellow-50 p-6 mt-10" onClick={()=> handleBuyCourse()}>
-                Buy Course
-            </button>
 
+        {confirmationModal && <ConfirmationModal modalData={confirmationModal} />}
         </div>
     )
 }
