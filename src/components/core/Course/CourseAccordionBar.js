1d0f25ab0d21c957f55962b5cf3baf8fc7c2d5f2
parthmern <parthmern@gmail.com>
2024-02-10 21:44:51 -0500

    updated

---
+++ src/components/core/Course/CourseAccordionBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CourseAccordionBar = () =>{
+
+    return(
+        <div>
+            CourseAccordionBar
+        </div>
+    )
+}
+
+export default CourseAccordionBar ;
--- src/pages/CourseDetails.jsx
+++ src/pages/CourseDetails.jsx
@@ -16,6 +16,8 @@ import ConfirmationModal from "../components/common/ConfirmationModal";
 import {fetchCourseDetails} from "../services/operations/courseDetailsAPI";
 import Error from "../pages/Error";
 
+import CourseAccordionBar from "../components/core/Course/CourseAccordionBar.js"
+
 const CourseDetails = () =>{
 
     // 1) load script
@@ -32,7 +34,20 @@ const CourseDetails = () =>{
 
     const [confirmationModal, setConfirmationModal] = useState(null);
 
+    const [response, setResponse] = useState(null)
+
+
     const [courseData, setCourseData] = useState(null);
+
+    const [isActive, setIsActive] = useState(Array(0))
+    const handleActive = (id) => {
+      // console.log("called", id)
+      setIsActive(
+        !isActive.includes(id)
+          ? isActive.concat([id])
+          : isActive.filter((e) => e != id)
+      )
+    }
 
     useEffect(()=>{
 
@@ -44,6 +59,7 @@ const CourseDetails = () =>{
                 const result = await fetchCourseDetails(courseId); 
                 console.log("result of getFUllcoursedetails=>", result);
                 setCourseData(result);
+                setResponse(result);
 
                 console.log("*************",result?.data[0] );
                 
@@ -204,6 +220,80 @@ const CourseDetails = () =>{
                 </div>
                 </div>
             </div>
+
+
+            
+
+        {/* ================ */}
+        <div className="mx-auto box-content px-4 text-start text-richblack-5 lg:w-[1260px]">
+            <div className="mx-auto max-w-maxContentTab lg:mx-0 xl:max-w-[810px]">
+            {/* What will you learn section */}
+            <div className="my-8 border border-richblack-600 p-8">
+                <p className="text-3xl font-semibold">What you'll learn</p>
+                <div className="mt-5">
+                {whatYouWillLearn}
+                </div>
+            </div>
+
+            {/* Course Content Section */}
+            <div className="max-w-[830px] ">
+                <div className="flex flex-col gap-3">
+                <p className="text-[28px] font-semibold">Course Content</p>
+                <div className="flex flex-wrap justify-between gap-2">
+                    <div className="flex gap-2">
+                    <span>
+                        {courseContent.length} {`section(s)`}
+                    </span>
+                    <span>
+                        {totalNoOfLectures} {`lecture(s)`}
+                    </span>
+                    <span>{response.data?.totalDuration} total length</span>
+                    </div>
+                    <div>
+                    <button
+                        className="text-yellow-25"
+                        onClick={() => setIsActive([])}
+                    >
+                        Collapse all sections
+                    </button>
+                    </div>
+                </div>
+                </div>
+
+                {/* Course Details Accordion */}
+                <div className="py-4">
+                {courseContent?.map((course, index) => (
+                    <CourseAccordionBar
+                    course={course}
+                    key={index}
+                    isActive={isActive}
+                    handleActive={handleActive}
+                    />
+                ))}
+                </div>
+
+                {/* Author Details */}
+                <div className="mb-12 py-4">
+                <p className="text-[28px] font-semibold">Author</p>
+                <div className="flex items-center gap-4 py-4">
+                    <img
+                    src={
+                        instructor.image
+                        ? instructor.image
+                        : `https://api.dicebear.com/5.x/initials/svg?seed=${instructor.firstName} ${instructor.lastName}`
+                    }
+                    alt="Author"
+                    className="h-14 w-14 rounded-full object-cover"
+                    />
+                    <p className="text-lg">{`${instructor.firstName} ${instructor.lastName}`}</p>
+                </div>
+                <p className="text-richblack-50">
+                    {instructor?.additionalDetails?.about}
+                </p>
+                </div>
+            </div>
+            </div>
+        </div>
 
 
         {confirmationModal && <ConfirmationModal modalData={confirmationModal} />}
