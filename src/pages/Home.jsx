ff8368db47935c214761c05cd9af8625cd57a058
parthmern <parthmern@gmail.com>
2024-02-14 11:28:29 -0500

    reviewSlider added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,7 @@ import LearningLanguageSection from "../components/core/HomePage/LearningLanguag
 import InstructorSection from "../components/core/HomePage/InstructorSection";
 import Footer from "../components/common/Footer";
 import ExploreMore from "../components/core/HomePage/ExploreMore";
+import { ReviewSlider } from "../components/common/ReviewSlider";
 
 
 const Home = () =>{
@@ -196,6 +197,7 @@ const Home = () =>{
                 </h2>
 
                 {/* review slider here */}
+                <ReviewSlider />
                 
 
             </div>
